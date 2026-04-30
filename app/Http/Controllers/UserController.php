c5cf14d0a6ac53e0c63103308f4bd2f21c51a51a
OlivierVencken <venckenolivier@gmail.com>
2026-04-30 17:07:57 +0200

    added session invalidation and regeneration on logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,6 +46,8 @@ class UserController extends Controller
     public function logout()
     {
         auth()->logout();
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
         return redirect('/');
     }
 }
